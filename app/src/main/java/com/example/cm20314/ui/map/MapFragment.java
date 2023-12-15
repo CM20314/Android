40e1fb0b15837de8fc63191681b7d14b3901b74a
ronaldobutrus <ronaldobutrus@gmail.com>
2023-12-15 02:58:42 +0000

    Configured fragments. Addressed #1, #2 and #3.

---
+++ app/src/main/java/com/example/cm20314/ui/map/MapFragment.java
@@ -0,0 +1,31 @@
+package com.example.cm20314.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.cm20314.databinding.FragmentMapBinding;
+
+public class MapFragment extends Fragment {
+
+    private FragmentMapBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        MapViewModel mapViewModel = new ViewModelProvider(this).get(MapViewModel.class);
+
+        binding = FragmentMapBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
