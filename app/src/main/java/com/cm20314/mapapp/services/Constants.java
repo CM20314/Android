f53331a1ff7aea4538ae345bed28ba2d44e1c83f
Isaac Pakanati <46679969+isaacpakanati@users.noreply.github.com>
2024-02-19 19:01:16 +0000

    Implemented favourites and recents

--- app/src/main/java/com/cm20314/mapapp/services/Constants.java
+++ app/src/main/java/com/cm20314/mapapp/services/Constants.java
@@ -2,4 +2,10 @@ package com.cm20314.mapapp.services;
 
 public class Constants {
     public static final String API_ROOT = "https://cm20314.azurewebsites.net/api";
+
+    public static final String FAVOURITES_SET_KEY = "favourites_set";
+
+    public static final String RECENTS1_KEY = "recents1";
+    public static final String RECENTS2_KEY = "recents2";
+    public static final String RECENTS3_KEY = "recents3";
 }
--- app/src/main/java/com/cm20314/mapapp/ui/map/MapViewModel.java
+++ app/src/main/java/com/cm20314/mapapp/ui/map/MapViewModel.java
@@ -4,16 +4,9 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.cm20314.mapapp.models.Container;
+
 public class MapViewModel extends ViewModel {
 
-    /*private final MutableLiveData<String> mText;
-
-    public MapViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }*/
+    public String destination;
 }
